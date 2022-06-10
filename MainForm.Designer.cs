0dcf816ed458adf625df4382aa4ed245b0845287
Aladser <aladser@gmail.com>
2022-06-10 17:30:50 +0900

    1.08

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -31,42 +31,47 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.selectLabel = new System.Windows.Forms.Label();
-            this.selectBox = new System.Windows.Forms.ComboBox();
+            this.selectQualityElement = new System.Windows.Forms.ComboBox();
             this.officeLabel = new System.Windows.Forms.Label();
             this.officeBox = new System.Windows.Forms.ComboBox();
             this.openButton = new System.Windows.Forms.Button();
             this.aboutImage = new System.Windows.Forms.PictureBox();
             this.infoField = new System.Windows.Forms.RichTextBox();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.selectedUnitsComboBox = new System.Windows.Forms.ComboBox();
+            this.filterButton = new System.Windows.Forms.Button();
+            this.windowLabel = new System.Windows.Forms.Label();
+            this.bw = new System.ComponentModel.BackgroundWorker();
+            this.progressLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.aboutImage)).BeginInit();
             this.SuspendLayout();
             // 
             // selectLabel
             // 
             this.selectLabel.AutoSize = true;
-            this.selectLabel.Location = new System.Drawing.Point(358, 12);
+            this.selectLabel.Location = new System.Drawing.Point(206, 17);
             this.selectLabel.Name = "selectLabel";
             this.selectLabel.Size = new System.Drawing.Size(44, 13);
             this.selectLabel.TabIndex = 0;
             this.selectLabel.Text = "Кнопка";
             // 
-            // selectBox
+            // selectQualityElement
             // 
-            this.selectBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.selectBox.FormattingEnabled = true;
-            this.selectBox.Items.AddRange(new object[] {
+            this.selectQualityElement.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.selectQualityElement.FormattingEnabled = true;
+            this.selectQualityElement.Items.AddRange(new object[] {
             "зеленая",
             "серая",
             "красная"});
-            this.selectBox.Location = new System.Drawing.Point(408, 9);
-            this.selectBox.Name = "selectBox";
-            this.selectBox.Size = new System.Drawing.Size(121, 21);
-            this.selectBox.TabIndex = 1;
+            this.selectQualityElement.Location = new System.Drawing.Point(256, 14);
+            this.selectQualityElement.Name = "selectQualityElement";
+            this.selectQualityElement.Size = new System.Drawing.Size(121, 21);
+            this.selectQualityElement.TabIndex = 1;
             // 
             // officeLabel
             // 
             this.officeLabel.AutoSize = true;
-            this.officeLabel.Location = new System.Drawing.Point(190, 12);
+            this.officeLabel.Location = new System.Drawing.Point(38, 17);
             this.officeLabel.Name = "officeLabel";
             this.officeLabel.Size = new System.Drawing.Size(35, 13);
             this.officeLabel.TabIndex = 2;
@@ -83,14 +88,14 @@
             "Офис 3",
             "Белогорск",
             "Свободный"});
-            this.officeBox.Location = new System.Drawing.Point(231, 10);
+            this.officeBox.Location = new System.Drawing.Point(79, 14);
             this.officeBox.Name = "officeBox";
             this.officeBox.Size = new System.Drawing.Size(121, 21);
             this.officeBox.TabIndex = 3;
             // 
             // openButton
             // 
-            this.openButton.Location = new System.Drawing.Point(535, 9);
+            this.openButton.Location = new System.Drawing.Point(383, 14);
             this.openButton.Name = "openButton";
             this.openButton.Size = new System.Drawing.Size(126, 21);
             this.openButton.TabIndex = 4;
@@ -101,7 +106,7 @@
             // aboutImage
             // 
             this.aboutImage.Image = ((System.Drawing.Image)(resources.GetObject("aboutImage.Image")));
-            this.aboutImage.Location = new System.Drawing.Point(879, 8);
+            this.aboutImage.Location = new System.Drawing.Point(879, 13);
             this.aboutImage.Name = "aboutImage";
             this.aboutImage.Size = new System.Drawing.Size(20, 22);
             this.aboutImage.TabIndex = 5;
@@ -116,17 +121,59 @@
             this.infoField.TabIndex = 6;
             this.infoField.Text = "";
             // 
+            // selectedUnitsComboBox
+            // 
+            this.selectedUnitsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.selectedUnitsComboBox.FormattingEnabled = true;
+            this.selectedUnitsComboBox.Items.AddRange(new object[] {
+            "Все"});
+            this.selectedUnitsComboBox.Location = new System.Drawing.Point(611, 14);
+            this.selectedUnitsComboBox.Name = "selectedUnitsComboBox";
+            this.selectedUnitsComboBox.Size = new System.Drawing.Size(121, 21);
+            this.selectedUnitsComboBox.TabIndex = 7;
+            // 
+            // filterButton
+            // 
+            this.filterButton.Enabled = false;
+            this.filterButton.Location = new System.Drawing.Point(738, 14);
+            this.filterButton.Name = "filterButton";
+            this.filterButton.Size = new System.Drawing.Size(75, 21);
+            this.filterButton.TabIndex = 8;
+            this.filterButton.Text = "Фильтр";
+            this.filterButton.UseVisualStyleBackColor = true;
+            // 
+            // windowLabel
+            // 
+            this.windowLabel.Location = new System.Drawing.Point(567, 17);
+            this.windowLabel.Name = "windowLabel";
+            this.windowLabel.Size = new System.Drawing.Size(38, 18);
+            this.windowLabel.TabIndex = 9;
+            this.windowLabel.Text = "Окна";
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(829, 17);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(16, 13);
+            this.progressLabel.TabIndex = 10;
+            this.progressLabel.Text = "   ";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(911, 428);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.windowLabel);
+            this.Controls.Add(this.filterButton);
+            this.Controls.Add(this.selectedUnitsComboBox);
             this.Controls.Add(this.infoField);
             this.Controls.Add(this.aboutImage);
             this.Controls.Add(this.openButton);
             this.Controls.Add(this.officeBox);
             this.Controls.Add(this.officeLabel);
-            this.Controls.Add(this.selectBox);
+            this.Controls.Add(this.selectQualityElement);
             this.Controls.Add(this.selectLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -141,13 +188,18 @@
         #endregion
 
         private System.Windows.Forms.Label selectLabel;
-        private System.Windows.Forms.ComboBox selectBox;
+        private System.Windows.Forms.ComboBox selectQualityElement;
         private System.Windows.Forms.Label officeLabel;
         private System.Windows.Forms.ComboBox officeBox;
         private System.Windows.Forms.Button openButton;
         private System.Windows.Forms.PictureBox aboutImage;
         private System.Windows.Forms.RichTextBox infoField;
         private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ComboBox selectedUnitsComboBox;
+        private System.Windows.Forms.Button filterButton;
+        private System.Windows.Forms.Label windowLabel;
+        private System.ComponentModel.BackgroundWorker bw;
+        private System.Windows.Forms.Label progressLabel;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -13,15 +13,27 @@ namespace PressKeySearch
 {
     public partial class MainForm : Form
     {
-        //String path = "D:\\EPMAgent0.log";
-        String path = "C:\\Users\\User\\Documents\\ProLAN\\EPMAgent\\EPMAgent0.log";
+        int li = 0;
+        int textLength; // число строк в EPMAgent0.log
+        String text = "";
+        String path = "D:\\EPMAgent0.log";
+        //String path = "C:\\Users\\User\\Documents\\ProLAN\\EPMAgent\\EPMAgent0.log";
         String[] nicks = { "псевдоним: \'1\'", "псевдоним: \'2\'", "псевдоним: \'3\'" };
-        String[] nameButton = {" Нажатие зеленой кнопки  ", " Нажатие серой кнопки  ", " Нажатие красной кнопки  "};
+        String[] qualities = {" Нажата зеленая кнопка  ", " Нажата серая кнопка  ", " Нажата красная кнопка  "};
         StreamReader sr;
 
         public MainForm()
         {
             InitializeComponent();
+
+            bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += bw_DoWork;
+            bw.ProgressChanged += bw_ProgressChanged;
+            bw.RunWorkerCompleted += bw_RunWorkerCompleted;
+
+
             if (!File.Exists(path))
             {
                 MessageBox.Show("Файл " + path + " не существует. Укажите путь к файлу");
@@ -36,33 +48,98 @@ namespace PressKeySearch
                     Environment.Exit(0);
                 }
             }
-            selectBox.SelectedIndex = 0;
+            textLength = System.IO.File.ReadAllLines(path).Length;
+            selectQualityElement.SelectedIndex = 0;
             officeBox.SelectedIndex = 0;
+            selectedUnitsComboBox.SelectedIndex = 0;
         }
 
         private void openButton_Click(object sender, EventArgs e)
         {
-            sr = new StreamReader(path, Encoding.Default);
-            String line;
-            String quality = nicks[selectBox.SelectedIndex];
-            String office = officeBox.Items[officeBox.SelectedIndex].ToString();
-            infoField.Text = "";
-            while ((line = sr.ReadLine()) != null)
+            if (bw.IsBusy != true)
             {
-                if (line.Contains(quality))
-                {
-                    if (officeBox.SelectedIndex==0 || line.Contains(office))
-                        if (line.Contains("Офис 1") || line.Contains("Офис 2") || line.Contains("Офис 3"))
-                            infoField.Text += line.Substring(0, 20) + nameButton[selectBox.SelectedIndex] + line.Substring(133, 6) + "  Окно " + line.Substring(125, 2) + "\n";
-                        else if (line.Contains("Белогорск") || line.Contains("Свободный"))
-                            infoField.Text += line.Substring(0, 20) + nameButton[selectBox.SelectedIndex] + line.Substring(125, 9) + "  " + line.Substring(135, 7) + "\n";
-                }
+                bw.RunWorkerAsync();
             }
         }
 
         private void aboutImage_MouseHover(object sender, EventArgs e)
         {
-            toolTip.SetToolTip(aboutImage, "(c) Aladser Версия 1.07 2022");
+            toolTip.SetToolTip(aboutImage, "(c) Aladser Версия 1.08 2022");
+        }
+        // тело backgroundworker
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            selectedUnitsComboBox.Invoke(new Action(() => { selectedUnitsComboBox.Items.Clear(); }));
+            selectedUnitsComboBox.Invoke(new Action(() => { selectedUnitsComboBox.Items.Add("Все"); }));
+            text = "";
+            infoField.Invoke(new Action(() => { infoField.Text = ""; }));
+            sr = new StreamReader(path, Encoding.Default);
+            String line;
+            String qualityName = ""; // выбранная оценка для поиска
+            String office = ""; // выбранный офис для поиска
+            selectQualityElement.Invoke(new Action(() => { qualityName = nicks[selectQualityElement.SelectedIndex]; }));
+            officeBox.Invoke(new Action(() => { office = officeBox.Items[officeBox.SelectedIndex].ToString(); }));
+
+            List<String> selectedWindows = new List<String>(); // Список
+            String selectedQuality = "";
+            String window = "";
+            int officeNumber = 0;
+            selectQualityElement.Invoke(new Action(() => { officeNumber = officeBox.SelectedIndex;  }));
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (line.Contains(qualityName))
+                {
+                    if (officeNumber == 0 || line.Contains(office))
+                    {
+                        selectQualityElement.Invoke(new Action(() => { selectedQuality = qualities[selectQualityElement.SelectedIndex]; }));
+                        if (line.Contains("Офис 1") || line.Contains("Офис 2") || line.Contains("Офис 3"))
+                        {
+                            window = line.Substring(133, 6) + " " + "Окно " + line.Substring(125, 2);
+                            text += line.Substring(0, 20) + selectedQuality + window + "\n";
+                        }
+                        else if (line.Contains("Белогорск") || line.Contains("Свободный"))
+                        {
+                            window = line.Substring(125, 9) + " " + line.Substring(135, 7);
+                            text += line.Substring(0, 20) + selectedQuality + window + "\n";    
+                        }
+                        if (!selectedWindows.Contains(window))
+                        {
+                            selectedWindows.Add(window);
+                        }
+                    }
+                }
+                //bw.ReportProgress(++li*100/textLength);
+            }
+            selectedWindows.Sort();
+            foreach (String elem in selectedWindows)
+            {
+                selectedUnitsComboBox.Invoke(new Action(() => { selectedUnitsComboBox.Items.Add(elem); }));
+            }
+        }
+        // обработка процесса backgroundworker
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressLabel.Text = e.ProgressPercentage + "%";
+        }
+
+        // Обработка окончания backgroundworker
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                progressLabel.Text = "Canceled!";
+            }
+            else if (e.Error != null)
+            {
+                progressLabel.Text = "Error: " + e.Error.Message;
+            }
+            else
+            {
+                progressLabel.Text = "Ок";
+                selectedUnitsComboBox.SelectedIndex = 0;
+                infoField.Text = text;
+                li = 0;
+            }
         }
     }
 }
